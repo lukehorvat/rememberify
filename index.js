509b5dd6dcfa325265228779f289eb2619591d75
Luke Horvat <lukehorvat@gmail.com>
2015-09-24 01:52:47 +1000

    Check for cache existence

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ var through = require("through2");
 var path = require("path");
 
 module.exports = function(b) {
+  var cache = b._options.cache;
+
+  if (cache) {
+    b.on("reset", reset);
+    reset();
+  }
+
   function reset() {
     b.pipeline.get("deps").push(through.obj(function(row, enc, callback) {
       var file = row.expose ? b._expose[row.id] : row.file;
-      b._options.cache[file] = { source: row.source, deps: objectAssign({}, row.deps) };
+      cache[file] = { source: row.source, deps: objectAssign({}, row.deps) };
       callback(null, row);
     }));
   }
-
-  b.on("reset", reset);
-
-  reset();
 
   return b;
 };
